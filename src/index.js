ffcf70d227b141b1296135ecc47365ea6f8404b8
kenny Egun <kennyegun240@gmail.com>
2022-10-18 21:47:22 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import './style.css';
-import clearInput from './modules/clearInput.js';
-import { getScores } from './modules/api';
-import { addScore } from './modules/api';
-import { score, namVal } from './modules/clearInput.js';
+import clearInput, { score, namVal } from './modules/clearInput.js';
+import { getScores, addScore } from './modules/api';
 
 const form = document.querySelector('.form');
 const refresh = document.querySelector('.refresh');
 
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-const scores = score.value
-const nameVal = namVal.value
+  const scores = score.value;
+  const nameVal = namVal.value;
 
-addScore(nameVal, scores);
-getScores();
-clearInput()
+  addScore(nameVal, scores);
+  getScores();
+  clearInput();
 });
 
-refresh.addEventListener('click', getScores)
+refresh.addEventListener('click', getScores);
